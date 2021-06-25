a8f47aeb19c317699c846f60857e66b16ca667ad
David <david.perspolis@gmail.com>
2021-06-25 07:10:22 -0500

    connect to postgre

--- app.py
+++ app.py
@@ -22,22 +22,66 @@ def get_inventory():
     '''
     return "<p>connect to database to get inventory  list </p>"
 
-if __name__=='__main__':
-     app.run('0.0.0.0','5000')
+# if __name__=='__main__':
+#      app.run('0.0.0.0','5000')
 
+
+
+import csv
+from types import new_class
 output_file_name="./doc/output.csv"
 input_file_name="./doc/nesting_example.csv"
 
+
+class Steel(): 
+    """ Steel class for size of steel 
+    """
+    def __init__(self,length,width,tickness,material) -> None:
+        self.length=length
+        self.width=width
+        self.tickness=tickness
+        self.material=material
+        
+        pass
+    def get_steel(self)->list:
+
+        return [self.length,self.width,self.tickness,self.material]
+    
+
+class Part(): 
+    """ Part class 
+    """
+    def __init__(self,name) -> None:
+        self.name
+        pass
+
 def read():
+    nesting=dict()
     with open(output_file_name, 'w', newline='') as fOut:
         writer = csv.writer(fOut)
         with open(input_file_name, newline='') as f:
             reader = csv.reader(f)
-            for row in reader:
-                sum=0
-                count=0
-                for score in row[1:]:
-                    sum+=int(score)
-                    count+=1
-                writer.writerow([row[0],str(sum/count)])
+            for i in reader:
+                nesting[i[0]]=i[1:]
+            nesting_name=nesting["nesting_name"]
+            # '''>>> print(nesting_name)
+            # ['P202120', '', '', '', '']'''
+            steel_used=Steel(nesting["length"][0],nesting['width'][0],tickness=nesting["thickness"][0],\
+                material=nesting["material"][0])
+            steel_rest = Steel(nesting["rest_size"][0], nesting["rest_size"][1], tickness=nesting["thickness"][0],\
+                material=nesting["material"][0])
+            writer.writerow(steel_used.get_steel())
+            writer.writerow(steel_rest.get_steel()) 
 
+
+#read()
+
+
+
+
+
+import psycopg2
+
+conn = psycopg2.connect(database="postgres", user = "postgres", password = "pass", host = "127.0.0.1", port = "5432")
+
+print ("Opened database successfully")
